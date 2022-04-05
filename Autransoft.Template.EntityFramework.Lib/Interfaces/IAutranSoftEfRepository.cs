0f282454536e7607ffc6181fbcd4ff52b14515ec
thiago <thiagoautran@gmail.com>
2022-04-04 23:01:27 -0300

    .

--- Autransoft.Template.EntityFramework.Lib/Interfaces/IAutranSoftEfRepository.cs
+++ Autransoft.Template.EntityFramework.Lib/Interfaces/IAutranSoftEfRepository.cs
@@ -3,11 +3,13 @@ using Autransoft.Template.EntityFramework.Lib.Entities;
 
 namespace Autransoft.Template.EntityFramework.Lib.Interfaces
 {
-    public interface IAutranSoftEfRepository<Entity>
+    public interface IAutranSoftEfRepository<Entity> : IAutranSoftEfRepository
         where Entity : AutranSoftEntity
     {
         Task<Entity> AddAsync(Entity entity);
 
         Task UpdateAsync(Entity entity);
     }
+
+    public interface IAutranSoftEfRepository { }
 }
